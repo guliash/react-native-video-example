c5232536e0b13ac5382bf06e341e69a0f28df100
Artem Gilmudinov <guliash92@gmail.com>
2019-02-03 15:54:20 +0300

    muted with fullscreen;

---
+++ App.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Video from 'react-native-video';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Video
+          source={ { uri: 'https://raw.githubusercontent.com/react-native-community/react-native-video/master/examples/basic/broadchurch.mp4' } }
+          style={ styles.video }
+          muted={ true }
+          ref={ player => this.player = player }
+        />
+
+        <TouchableOpacity
+          onPress={ () => {
+            if (this.player) {
+              this.player.presentFullscreenPlayer();
+            }
+          } }>
+          <Text>Fullscreen</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF'
+  },
+  video: {
+    height: 300
+  }
+});
